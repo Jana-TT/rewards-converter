0315cbeeffa1c4897b4587b5ddc37fbc091b3891
Jana-TT <janatt2056@gmail.com>
2023-08-19 13:46:59 -0500

    added unit tests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,18 +1,21 @@
 
 public class RewardValue {
-    
+
     int miles;
+    double cash;
     
     public RewardValue(double cashValue) {
        miles = (int) (cashValue / 0.0035);
+       cash = cashValue;
     }
 
     public RewardValue(int milesValue) {
        miles = milesValue;
+       cash = miles * 0.0035;
     }
 
     public double getCashValue() {
-        return miles * 0.0035;
+        return cash;
     }
 
     public int getMilesValue() {
